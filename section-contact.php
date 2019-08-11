29aca3935058b4037da266e32971ad41e998aef2
Jake Causier <jake@purplemountmedia.com>
2019-08-11 18:09:49 +0100

    Changed the breakpoint size for e-mail address in the contact form section

--- section-contact.php
+++ section-contact.php
@@ -8,13 +8,13 @@
 		</div>
 		<div class="col-12 <?php echo (get_post_meta(get_the_ID(), 'page_contact_form_first', true) === 'on' ? 'order-3' : 'order-2'); ?>">
 			<div class="row">
-				<div class="col-12 col-md-6 mb-4">
+				<div class="col-12 col-lg-6 mb-4">
 					<a href="tel:<?php echo gardening_get_option('contact_phone'); ?>" aria-label="Call me at <?php echo gardening_get_option('contact_phone'); ?>" class="contact-item">
 						<i class="fas fa-2x fa-phone mb-4"></i>
 						<span><?php echo gardening_get_option('contact_phone'); ?></span>
 					</a>
 				</div>
-				<div class="col-12 col-md-6 mb-4">
+				<div class="col-12 col-lg-6 mb-4">
 					<a href="mailto:<?php echo gardening_get_option('contact_email'); ?>" aria-label="E-mail me at <?php echo gardening_get_option('contact_email'); ?>" class="contact-item">
 						<i class="fas fa-2x fa-envelope mb-4"></i>
 						<span><?php echo gardening_get_option('contact_email'); ?></span>
